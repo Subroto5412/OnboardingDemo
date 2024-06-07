89bd30d73b8695842888bd429efe550f9083bd97
subroto <subroto_mohonto@yahoo.com>
2024-06-07 22:17:37 +0600

    HorizontalPagerContent data class

---
+++ app/src/main/java/com/bd/onboardingdemo/HorizontalPagerContent.kt
@@ -0,0 +1,18 @@
+package com.bd.onboardingdemo
+
+import androidx.annotation.DrawableRes
+
+data class HorizontalPagerContent(
+    val title: String,
+    @DrawableRes val res: Int,
+    val desc: String
+)
+
+fun getList(): List<HorizontalPagerContent> {
+    return listOf(
+        HorizontalPagerContent("Verified", R.drawable.first,"Verification is an extra or final bit of proof that establishes something is true"),
+        HorizontalPagerContent("Make it simple", R.drawable.second,"We pay attention to all of your payments and find way for saving your money"),
+        HorizontalPagerContent("New Banking", R.drawable.third,"Free Advisory service,mobile banking application,visa"),
+        HorizontalPagerContent("Verified", R.drawable.fourth,"Bank your life,We create something new you have never seen before")
+    )
+}
